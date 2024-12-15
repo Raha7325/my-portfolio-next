d96dd4d9fe1d92740f84b896a15658a902bebf7e
Raha7325 <f.keshir25@gmail.com>
2024-12-15 12:15:34 +0330

    updated package-json file and install gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    assetPrefix: isProd ? 'https://github.com/Raha7325/my-portfolio' : '',
+    basePath: isProd ? 'https://github.com/Raha7325/my-portfolio' : '',
+    output: 'export'
+};
 
 export default nextConfig;
