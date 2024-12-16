c5aebde3dd8db82c743d06afdc36bba084c345fb
Raha7325 <f.keshir25@gmail.com>
2024-12-16 21:52:58 +0330

    update about content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,17 +1,13 @@
 const About = () => {
     return (
-        <h1 className="mt-[94px] mx-[35px]">
-            I am a dedicated front-end developer with
-            expertise in React.js and Vue.js,
-            passionate about crafting seamless and
-            dynamic user experiences. Continuously
-            seeking to expand my knowledge, I
-            embrace new challenges and
-            technologies to enhance my skills and
-            contribute effectively to my projects. My
-            goal is to consistently deliver high-quality,
-            innovative solutions while driving my
-            professional growth.
+        <h1 className="mt-[94px] mx-[35px] text-justify">
+            I'm Raha, a passionate web developer with a love for crafting seamless and engaging user experiences.
+
+            I specialize in designing and building user interfaces (UI) that are not only visually appealing but also highly functional. Currently, I work with React and Vue.js, leveraging these powerful frameworks to bring ideas to life. My journey in web development is fueled by a constant drive to learn, improve, and stay updated with the latest technologies.
+
+            For me, coding is more than just writing lines of code—it's about combining creativity and logic to create something meaningful that can make people's lives easier or more delightful.
+
+            When I'm not coding, you'll probably find me enjoying nature, diving into a good book, or exploring new creative challenges. My ultimate goal is to grow as a developer while contributing to the vibrant and ever-evolving world of the web.
         </h1>
     )
 }
