f121e7eda9188a15c9f8cb0a0cc6666686a52411
Raha7325 <f.keshir25@gmail.com>
2024-12-17 12:12:16 +0330

    change next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/my-portfolio' : '',
+    output: 'export',
     reactStrictMode: true,
     images: {
         unoptimized: true,
     },
     assetPrefix: isProd ? '/my-portfolio/' : '',
-    basePath: isProd ? '/my-portfolio' : '',
-    output: 'export'
 };
 
-export default nextConfig;
+module.exports = nextConfig;
