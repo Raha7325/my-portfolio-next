ce5930a90a3c20821561513baeeb5fc515237259
Raha7325 <f.keshir25@gmail.com>
2024-12-20 13:30:28 +0330

    change background image path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import useConfig from '../hooks/useConfig';
 
 const Header = () => {
-    const { basePath } = useConfig();
     return (
         <nav>
             <div className="max-w-screen-xl px-4 py-3 mx-auto flex justify-between">
                 <Link href="/" className="cursor-pointer">
                     <Image src='./images/Logo.png' className="rounded-full object-cover h-[70px]" alt="Faezeh(raha) kashir" width={70} height={70} />
                 </Link>
-                {basePath}
                 <div className="flex items-center">
                     <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
                         <li>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,9 +5,10 @@ import Footer from './Footer';
 const Layout = ({ children }) => {
     const router = useRouter();
 
-    const background = router.pathname === "/" ? "background-homePage" : "bg-pink-100"
+    const background = router.pathname === "/" ? "background-homePage " : "bg-pink-100"
+    console.log(background)
     return (
-        <div className={`flex flex-col min-h-screen ${background}`} style={{ backgroundImage: `url('./images/background-homePage.jpeg')` }}>
+        <div className={`flex flex-col min-h-screen ${background}`} >
             <Header />
             <main className="flex-grow">
                 <div className="container mx-auto p-4">
