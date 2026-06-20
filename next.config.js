b11bf7294a8ecad1a1c3bfe21ba002a62de621cd
Raha7325 <f.keshir25@gmail.com>
2026-06-20 10:42:26 +0330

    fix: correct basePath to match repo name my-portfolio-next

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    basePath: isProd ? '/my-portfolio' : '',
+    basePath: isProd ? '/my-portfolio-next' : '',
     output: 'export',
     reactStrictMode: true,
     images: {
